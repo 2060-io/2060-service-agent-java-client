bec4c1dfae8d78234172d8bd5b068df8ad74694c
Andres Vallecilla <andresfelipe083195@hotmail.com>
2024-09-27 09:39:04 -0500

    fix: remove unused dependency

--- src/main/java/io/twentysixty/sa/client/model/message/calls/CallAcceptRequestMessage.java
+++ src/main/java/io/twentysixty/sa/client/model/message/calls/CallAcceptRequestMessage.java
@@ -9,13 +9,11 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import io.twentysixty.sa.client.model.message.BaseMessage;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 @JsonInclude(Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Setter
 @Getter
-@ToString
 public class CallAcceptRequestMessage extends BaseMessage {
 
 	private static final long serialVersionUID = -2840211856886973682L;
--- src/main/java/io/twentysixty/sa/client/model/message/calls/CallRejectRequestMessage.java
+++ src/main/java/io/twentysixty/sa/client/model/message/calls/CallRejectRequestMessage.java
@@ -5,15 +5,9 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 import io.twentysixty.sa.client.model.message.BaseMessage;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
 
 @JsonInclude(Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
-@Setter
-@Getter
-@ToString
 public class CallRejectRequestMessage extends BaseMessage {
 
 	private static final long serialVersionUID = -2840211856886973682L;
