5101a5e93b4085db02b12828b9b8b625205ba167
Andres Vallecilla <andresfelipe083195@hotmail.com>
2024-10-03 15:25:29 -0500

    test

--- src/main/java/io/twentysixty/sa/client/jms/ProducerInterface.java
+++ src/main/java/io/twentysixty/sa/client/jms/ProducerInterface.java
@@ -11,7 +11,6 @@ public interface ProducerInterface<M extends Serializable> {
 	public void setThreads(Integer threads);
 
 	public void setDebug(Boolean debug);
-
 	public void sendMessage(M message) throws Exception;
 
 }
