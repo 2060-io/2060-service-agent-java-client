129aaffd207b35277b1511ce7b3fceec9c8d3361
Andres Vallecilla <andresfelipe083195@hotmail.com>
2024-10-04 11:18:27 -0500

    refractor: formatter

--- src/main/java/io/twentysixty/sa/client/I18n/InternationalizationService.java
+++ src/main/java/io/twentysixty/sa/client/I18n/InternationalizationService.java
@@ -7,78 +7,77 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.UUID;
-
 import org.eclipse.microprofile.config.ConfigProvider;
 
 public class InternationalizationService {
 
-	private String defaultLanguage = "en";
-	private List<String> messages = new ArrayList<>();
-	private String rootFiles = "META-INF/resources/Messages";
+  private String defaultLanguage = "en";
+  private List<String> messages = new ArrayList<>();
+  private String rootFiles = "META-INF/resources/Messages";
 
-	Map<UUID, ResourceBundle> bundles = new HashMap<>();
+  Map<UUID, ResourceBundle> bundles = new HashMap<>();
 
-	public String get(String messageName, UUID connection, String language, ClassLoader classLoader) {
-		String retval = messageName;
-		ResourceBundle currentBundle;
+  public String get(String messageName, UUID connection, String language, ClassLoader classLoader) {
+    String retval = messageName;
+    ResourceBundle currentBundle;
 
-		if (!bundles.containsKey(connection)) {
-			if (language != null && !language.isEmpty()) {
-				try {
-					currentBundle = ResourceBundle.getBundle(getRootFiles(), new Locale(language), classLoader);
-				} catch (Exception e) {
-					currentBundle = ResourceBundle.getBundle(getRootFiles(), new Locale(getDefaultLanguage()));
-				}
-			} else {
-				currentBundle = ResourceBundle.getBundle(getRootFiles(), new Locale(getDefaultLanguage()));
-			}
+    if (!bundles.containsKey(connection)) {
+      if (language != null && !language.isEmpty()) {
+        try {
+          currentBundle =
+              ResourceBundle.getBundle(getRootFiles(), new Locale(language), classLoader);
+        } catch (Exception e) {
+          currentBundle =
+              ResourceBundle.getBundle(getRootFiles(), new Locale(getDefaultLanguage()));
+        }
+      } else {
+        currentBundle = ResourceBundle.getBundle(getRootFiles(), new Locale(getDefaultLanguage()));
+      }
 
-			bundles.put(connection, currentBundle);
-		} else {
-			currentBundle = bundles.get(connection);
-		}
+      bundles.put(connection, currentBundle);
+    } else {
+      currentBundle = bundles.get(connection);
+    }
 
-		try {
-			retval = currentBundle.getString(messageName);
-		} catch (Exception e) {
+    try {
+      retval = currentBundle.getString(messageName);
+    } catch (Exception e) {
 
-		}
+    }
 
-		try {
-			if (getMessages().contains(messageName))
-				return this.getConfigValue(retval);
-		} catch (Exception e) {
-		}
+    try {
+      if (getMessages().contains(messageName)) return this.getConfigValue(retval);
+    } catch (Exception e) {
+    }
 
-		return retval;
-	}
+    return retval;
+  }
 
-	private String getConfigValue(String key) {
-		return ConfigProvider.getConfig().getValue(key, String.class);
-	}
+  private String getConfigValue(String key) {
+    return ConfigProvider.getConfig().getValue(key, String.class);
+  }
 
-	public String getDefaultLanguage() {
-		return defaultLanguage;
-	}
+  public String getDefaultLanguage() {
+    return defaultLanguage;
+  }
 
-	public void setDefaultLanguage(String defaultLanguage) {
-		this.defaultLanguage = defaultLanguage;
-	}
+  public void setDefaultLanguage(String defaultLanguage) {
+    this.defaultLanguage = defaultLanguage;
+  }
 
-	public List<String> getMessages() {
-		return messages;
-	}
+  public List<String> getMessages() {
+    return messages;
+  }
 
-	public void setMessages(List<String> messages) {
-		this.messages = messages;
-	}
+  public void setMessages(List<String> messages) {
+    this.messages = messages;
+  }
 
-	public String getRootFiles() {
-		return rootFiles;
-	}
+  public String getRootFiles() {
+    return rootFiles;
+  }
 
-	public void setRootFiles(String rootFiles) {
-		this.rootFiles = rootFiles;
-	}
-
+  public void setRootFiles(String rootFiles) {
+    this.rootFiles = rootFiles;
+  }
 }
