4a0fe2919d7026a02036876ab9608fe687cce6fe
Andres Vallecilla <andresfelipe083195@hotmail.com>
2024-10-23 02:04:38 -0500

    fix: new typing

--- src/main/java/io/twentysixty/sa/client/model/message/mrtd/EMrtdData.java
+++ src/main/java/io/twentysixty/sa/client/model/message/mrtd/EMrtdData.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import java.io.Serializable;
+import java.util.List;
 import java.util.Map;
 import lombok.Getter;
 import lombok.Setter;
@@ -27,7 +28,9 @@ public class EMrtdData implements Serializable {
   @JsonInclude(Include.NON_NULL)
   @JsonIgnoreProperties(ignoreUnknown = true)
   public static class EMrtdProcessedData {
-    private ProcessedData fields;
+    private EF_DG1 ef_dg1;
+    private EF_DG2 ef_dg2;
+    private EF_DG11 ef_dg11;
   }
 
   @Getter
@@ -35,14 +38,45 @@ public class EMrtdData implements Serializable {
   @ToString
   @JsonInclude(Include.NON_NULL)
   @JsonIgnoreProperties(ignoreUnknown = true)
-  public static class ProcessedData {
-    private String mrzData;
-    private String firstName;
+  public static class EF_DG1 {
+    private String documentType;
+    private String documentNumber;
+    private String issuingState;
+    private String dateOfBirth;
+    private String dateOfExpiry;
+    private String sex;
+    private String nationality;
     private String lastName;
-    private String faceDataUrl;
-    private String fingerprintDataUrl;
-    private long birthDate;
-    private String placeOfBirth;
-    private long issuanceDate;
+    private String firstName;
+    private String nameOfHolder;
+    private String mrzOptionalData;
+  }
+
+  @Getter
+  @Setter
+  @ToString
+  @JsonInclude(Include.NON_NULL)
+  @JsonIgnoreProperties(ignoreUnknown = true)
+  public static class EF_DG2 {
+    private List<String> faceImages;
+  }
+
+  @Getter
+  @Setter
+  @ToString
+  @JsonInclude(Include.NON_NULL)
+  @JsonIgnoreProperties(ignoreUnknown = true)
+  public static class EF_DG11 {
+    private String nameOfHolder;
+    private Long dateOfBirth;
+    private List<String> otherNames;
+    private String personalNumber;
+    private List<String> placeOfBirth;
+    private List<String> permanentAddress;
+    private String telephone;
+    private String profession;
+    private String title;
+    private String personalSummary;
+    private String custodyInformation;
   }
 }
